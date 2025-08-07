fa2e9a90861c41cde4cb48a3debc0e64c589de77
adhikariprajitraj <prajit.raj.adhikari@gmail.com>
2025-08-07 22:02:31 +0545

    Added the package, cool stuff!

---
+++ kellycalc/__init__.py
@@ -0,0 +1,9 @@
+# Well this just serves as package, cool concept llms has taught me
+
+
+__version__ = "0.1.0"
+
+from .core import MultiOutcomeKelly #, PortfolioSimulator TODO: Add this later.
+# TODO: from .viz import plot_bankroll
+
+__all__ = ["MultiOutcomeKelly"]
